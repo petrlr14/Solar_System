ce53bac18adc32040ddee01047e3f9551653cc1e
petrlr14 <00057616@uca.edu.sv>
2018-04-14 20:24:43 -0600

    se agregó el elemento sol a todos los lugares necesarios

--- app/src/main/java/com/pdm00057616/solarsystem/Planet.java
+++ app/src/main/java/com/pdm00057616/solarsystem/Planet.java
@@ -42,14 +42,15 @@ public class Planet implements Serializable{
 
 
     private void setArrayID(){
-        imageViewHashMap.put(0, R.drawable.mercury_1);
-        imageViewHashMap.put(1, R.drawable.venus_2);
-        imageViewHashMap.put(2, R.drawable.earth_3);
-        imageViewHashMap.put(3, R.drawable.mars_4);
-        imageViewHashMap.put(4, R.drawable.jupiter_5);
-        imageViewHashMap.put(5, R.drawable.saturn_6);
-        imageViewHashMap.put(6, R.drawable.uranos_7);
-        imageViewHashMap.put(7, R.drawable.neptune_8);
+        imageViewHashMap.put(0, R.drawable.sun_0);
+        imageViewHashMap.put(1, R.drawable.mercury_1);
+        imageViewHashMap.put(2, R.drawable.venus_2);
+        imageViewHashMap.put(3, R.drawable.earth_3);
+        imageViewHashMap.put(4, R.drawable.mars_4);
+        imageViewHashMap.put(5, R.drawable.jupiter_5);
+        imageViewHashMap.put(6, R.drawable.saturn_6);
+        imageViewHashMap.put(7, R.drawable.uranos_7);
+        imageViewHashMap.put(8, R.drawable.neptune_8);
     }
 
 
@@ -58,14 +59,15 @@ public class Planet implements Serializable{
      */
     public int getContent(){
         switch (indice){
-            case 0: return R.string.Mercury;
-            case 1: return R.string.Venus;
-            case 2: return R.string.Earth;
-            case 3: return R.string.Mars;
-            case 4: return R.string.Jupiter;
-            case 5: return R.string.Saturn;
-            case 6: return R.string.Uranus;
-            case 7: return R.string.Neptune;
+            case 0: return R.string.Sun;
+            case 1: return R.string.Mercury;
+            case 2: return R.string.Venus;
+            case 3: return R.string.Earth;
+            case 4: return R.string.Mars;
+            case 5: return R.string.Jupiter;
+            case 6: return R.string.Saturn;
+            case 7: return R.string.Uranus;
+            case 8: return R.string.Neptune;
         }
         return 10;
     }
@@ -77,14 +79,15 @@ public class Planet implements Serializable{
      */
     public int getColor(){
         switch (indice){
-            case 0: return R.color.Mercury;
-            case 1: return R.color.Venus;
-            case 2: return R.color.Earth;
-            case 3: return R.color.Mars;
-            case 4: return R.color.Jupiter;
-            case 5: return R.color.Saturn;
-            case 6: return R.color.Uranus;
-            case 7: return R.color.Neptune;
+            case 0: return R.color.Sun;
+            case 1: return R.color.Mercury;
+            case 2: return R.color.Venus;
+            case 3: return R.color.Earth;
+            case 4: return R.color.Mars;
+            case 5: return R.color.Jupiter;
+            case 6: return R.color.Saturn;
+            case 7: return R.color.Uranus;
+            case 8: return R.color.Neptune;
         }
         return 10;
     }
